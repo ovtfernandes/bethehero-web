e4480b4e80263eed4b4e13f160f42f61a135870a
Vitor Fernandes <s.fernandes.vitor@gmail.com>
2020-03-25 16:22:44 -0300

    Adds register page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Login from './pages/login';
+import Register from './pages/register';
 
 function Routes() {
     return (
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Login} />
+                <Route path="/" exact component={Login} />
+                <Route path="/register" component={Register} />
             </Switch>
         </BrowserRouter>
     );
